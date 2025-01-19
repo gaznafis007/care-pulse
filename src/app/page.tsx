626741d81d879433c33f11ddc496f77087796a81
unknown <gazinafisrafi.gnr@gmail.com>
2025-01-19 22:51:42 +0600

    build:welcome page designed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,11 @@ import Image from "next/image";
 import logo from "../assets/logo.png";
 import welcome from "../assets/login-illustration.png";
 import InputField from "@/components/InputField/InputField";
+import Button from "@/components/Button/Button";
 
 export default function Home() {
   return (
-    <main className="rounded-tr-xl">
+    <main className="relative">
       {/* logo */}
       <div className="flex flex-row space-x-2 text-white items-center pl-16 mt-12">
         <Image alt="logo" src={logo} width={40} height={40} />
@@ -17,14 +18,17 @@ export default function Home() {
           <h2 className="text-3xl font-semibold text-white"> Hi there, ...</h2>
           <h3 className="text-slate-300">Get Started with Appointments</h3>
           </div>
-          <form className="flex flex-col pr-16 space-y-2">
-            <InputField label={'Full Name'} labelClass={'text-lg'} className={'bg-slate-800'} name={'name'} type={'text'} placeholder={'e.g. Enter your full name'}/>
-            <InputField label={'Email Address'} labelClass={'text-lg'} className={'bg-slate-800'} name={'email'} type={'email'} placeholder={'e.g. johndoe@gmail.com'}/>
-            <InputField label={'Phone Number'} labelClass={'text-lg'} className={'bg-slate-800'} name={'phone'} type={'text'} placeholder={'e.g. 012xxxxxxxx'}/>
+          <form className="flex flex-col pr-16 space-y-4">
+            <InputField label={'Full Name'} labelClass={'text-lg text-slate-300'} className={'bg-slate-800'} name={'name'} type={'text'} placeholder={'e.g. Enter your full name'}/>
+            <InputField label={'Email Address'} labelClass={'text-lg text-slate-300'} className={'bg-slate-800'} name={'email'} type={'email'} placeholder={'e.g. johndoe@gmail.com'}/>
+            <InputField label={'Phone Number'} labelClass={'text-lg text-slate-300'} className={'bg-slate-800'} name={'phone'} type={'text'} placeholder={'e.g. 012xxxxxxxx'}/>
+            <div className="flex flex-col mt-12">
+            <Button className={'block'}> Get Started</Button>
+            </div>
           </form>
         </div>
         {/* image */}
-        <div className="h-[750px] w-full md:w-1/2 -mt-24 hidden md:block">
+        <div className="md:h-[796px] w-full md:w-1/2 -mt-24 hidden md:block">
           <Image
             src={welcome}
             alt="welcome-illustration"
@@ -34,6 +38,7 @@ export default function Home() {
           />
         </div>
       </div>
+      <p className="absolute pl-16 -bottom-12 md:bottom-2 text-slate-400">@carpulse copywrite</p>
     </main>
   );
 }
