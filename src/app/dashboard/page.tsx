34f6da17d80009c45a8d5dad7cbedab99b4c3899
unknown <gazinafisrafi.gnr@gmail.com>
2025-01-20 19:46:37 +0600

    maintaining the streak for the last time

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+function Dashboard({}: Props) {
+  return (
+    <div>
+        <h2 className="text-indigo-500 text-4xl text-center p-4">This is dashboard</h2>
+    </div>
+  )
+}
+
+export default Dashboard
