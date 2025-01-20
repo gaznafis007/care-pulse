4962b85bf30c96ca331b8b338993fcb582dbbccc
unknown <gazinafisrafi.gnr@gmail.com>
2025-01-20 19:55:51 +0600

    dashboard navbar fixing

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 const Register = () => {
     return (
         <div>
-            <div className="flex flex-row space-x-4">
+            <div className="flex flex-row space-x-2 items-center pl-16 mt-12">
                 <Image src={logo} alt='company-logo' width={40} height={40}/>
                 <p className="text-2xl font-semibold">CarePulse</p>
             </div>
