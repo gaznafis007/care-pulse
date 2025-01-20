c0f07abcc04068e19cc94e22468ccf40c5e202fd
unknown <gazinafisrafi.gnr@gmail.com>
2025-01-20 19:53:29 +0600

    dashboard navbar init

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React from "react";
+import logo from "../../assets/logo.png";
+import Image from "next/image";
 
-type Props = {}
+type Props = {};
 
 function Dashboard({}: Props) {
   return (
     <div>
-        <h2 className="text-indigo-500 text-4xl font-semibold text-center p-4">This is dashboard</h2>
+      <div className="flex flex-row space-x-4">
+        <Image src={logo} alt="company logo" width={40} height={40} />
+        <p className="text-2xl font-semibold">CarePulse</p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
