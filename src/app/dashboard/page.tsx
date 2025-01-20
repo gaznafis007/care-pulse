640bd27214e1f7bd99c0821a2fa00b6ccf9960c1
unknown <gazinafisrafi.gnr@gmail.com>
2025-01-20 19:49:06 +0600

    changes

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,7 +5,7 @@ type Props = {}
 function Dashboard({}: Props) {
   return (
     <div>
-        <h2 className="text-indigo-500 text-4xl text-center p-4">This is dashboard</h2>
+        <h2 className="text-indigo-500 text-4xl font-semibold text-center p-4">This is dashboard</h2>
     </div>
   )
 }
