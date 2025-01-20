5c36513728117e155745f126ecc837e74189b6d1
unknown <gazinafisrafi.gnr@gmail.com>
2025-01-20 19:56:37 +0600

    register navbar fixing

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -7,7 +7,7 @@ type Props = {};
 function Dashboard({}: Props) {
   return (
     <div>
-      <div className="flex flex-row space-x-4 items-center pl-16 mt-12">
+      <div className="flex flex-row space-x-2 items-center pl-16 mt-12">
         <Image src={logo} alt="company logo" width={40} height={40} />
         <p className="text-2xl font-semibold">CarePulse</p>
       </div>
